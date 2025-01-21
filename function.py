f69f356071c32b416a1395ea3050bd6ff8fb12fb
Sidnei de Oliveira Melania <sidnei.melania@gmail.com>
2025-01-20 23:45:58 -0300

    Python Puro

---
+++ function.py
@@ -0,0 +1,42 @@
+def add_subscription(self):
+ empresa = input('Empresa: ')
+ site = input('Site: ')
+ data_assinatura = datetime.strptime(input('Data de assinatura: '), '%d/%m/%Y')
+ valor = Decimal(input('Valor: '))
+ subscription = Subscription(empresa=empresa, site=site, data_assinatura=data_assinatura, valor=valor)
+ self.subscription_service.create(subscription)
+ print('Assinatura adicionada com sucesso.')
+
+def delete_subscription(self):
+ subscriptions = self.subscription_service.list_all()
+ print('Escolha qual assinatura deseja excluir')
+ for i in subscriptions:
+ print(f'[{i.id}] -> {i.empresa}')
+ choice = int(input('Escolha a assinatura: '))
+ self.subscription_service.delete(choice)
+ print('Assinatura excluída com sucesso.')
+
+def total_value(self):
+ print(f'Seu valor total mensal em assinaturas: {self.subscription_service.total_value()}')
+
+def start(self):
+ while True:
+     print('''
+        [1] -> Adicionar assinatura
+        [2] -> Remover assinatura
+        [3] -> Valor total
+        [4] -> Gastos últimos 12 meses
+        [5] -> Sair
+           ''')
+     choice = int(input('Escolha uma opção: '))
+     if choice == 1:
+        self.add_subscription()
+     elif choice == 2:
+            self.delete_subscription()
+     elif choice == 3:
+            self.total_value()
+     elif choice == 4:
+            self.subscription_service.gen_chart()
+     else:
+
+        break
